da9929ad9df7bb38db9e79a6d9cbd73204f363df
Anku Kumari <ankuhere96@gmail.com>
2023-09-11 16:27:05 +0530

    Added hamburger

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -9,12 +9,31 @@ function Header() {
           <a className="navbar-button">About</a>
           <a className="navbar-button">Services</a>
         </div>
-        <Image src={Logo} className="header-logo" alt="Logo of the company"/>
+        <Image src={Logo} className="header-logo" alt="Logo of the company" />
         <div className="navOptions-left">
           <a className="navbar-button">Enquire</a>
           <a className="navbar-button">Contact</a>
         </div>
       </div>
+      {/* <div> */}
+      <section class="top-nav">
+        <div>
+          {" "}
+          <Image src={Logo} className="header-logo-mobile" alt="Logo of the company" />
+        </div>
+        <input id="menu-toggle" type="checkbox" />
+        <label class="menu-button-container" for="menu-toggle">
+          <div class="menu-button"></div>
+        </label>
+        <ul class="menu">
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+          <li>Four</li>
+          <li>Five</li>
+        </ul>
+      </section>
+      {/* </div> */}
     </header>
   );
 }
