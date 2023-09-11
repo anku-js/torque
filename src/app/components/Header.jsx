bbee966154eba740636052ecd98a58d73c043c68
Anku Kumari <ankuhere96@gmail.com>
2023-09-11 20:33:07 +0530

    modified logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -19,18 +19,21 @@ function Header() {
       <section class="top-nav">
         <div>
           {" "}
-          <Image src={Logo} className="header-logo-mobile" alt="Logo of the company" />
+          <Image
+            src={Logo}
+            className="header-logo-mobile"
+            alt="Logo of the company"
+          />
         </div>
         <input id="menu-toggle" type="checkbox" />
         <label class="menu-button-container" for="menu-toggle">
           <div class="menu-button"></div>
         </label>
         <ul class="menu">
-          <li>One</li>
-          <li>Two</li>
-          <li>Three</li>
-          <li>Four</li>
-          <li>Five</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Enquire</li>
+          <li>Contact</li>
         </ul>
       </section>
       {/* </div> */}
