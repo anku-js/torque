fd1948465ebfb192221df5aba7534b066dfa423e
Roshan Singh <singhroshan209@gmail.com>
2023-09-13 00:22:17 +0530

    refined the UI

--- src/app/components/Contacts/index.jsx
+++ src/app/components/Contacts/index.jsx
@@ -1,12 +1,17 @@
 import "./styles.css";
-import Image from "next/image";
-import Map from "../../../../public/images/Map.png";
 function Contacts() {
   return (
     <section className="contact-container">
       <div className="contact">
         <div className="contact-left">
-          <Image src={Map} className="map-image" alt="Map of the location" />
+          <div className="map-image">
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.9056183904536!2d73.03266107613597!3d19.02388015360486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3b3d93cca45%3A0xa1c0a3d24133c53!2sPriyanka%20Hill%20View%20Residency!5e0!3m2!1sen!2sin!4v1694544258803!5m2!1sen!2sin" 
+              allowFullScreen="" 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade">
+            </iframe>
+          </div>
         </div>
         <div className="contact-right">
           <div className="contact-text">
