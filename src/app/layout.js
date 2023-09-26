41fb80dfb9e36e998dacfa97b1d20a2769c751df
Anku Kumari <ankuhere96@gmail.com>
2023-09-26 17:13:07 +0530

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,14 +32,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* <link rel="icon" href="../../public/images/logo2.png" /> */}
         <link
           rel="icon"
           type="image/png"
           href="/images/favicon.png"
           sizes="32x32"
         />
-        {/* <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script> */}
       </head>
       <body
         className={`${ultra.variable} ${barlow.variable} ${stintUltra.variable} ${poppins.variable}`}
